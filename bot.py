270933a8f6ac77a492ca7d5495d42ee9b3fd3c81
Noah <noahpdx@gmail.com>
2021-05-17 20:59:04 -0700

    Added echo command, basic api usage, shutdown command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import requests
 
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -10,13 +11,43 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
+# On join
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    print(f'{bot.user} connected to Discord.')
 
+
+# Commands
 @bot.command(name='hb')
 async def happy_birthday(ctx):
     response = 'Happy Birthday! 🎈🎉'
     await ctx.send(response)
 
+@bot.command(name='api')
+async def api_test(ctx):
+    response = requests.get('https://pokeapi.co/api/v2/berry/1')
+    await ctx.send("Attempted.")
+    await ctx.send(response.status_code)
+    if response.status_code == 200:
+        #j = response.json() need to fix json parsing
+        #await ctx.send(j.name) 
+        await ctx.send("Success.")
+    elif response.status_code > 400:
+        await ctx.send('Connection failed.')
+        await ctx.send('Status code: ', response.status_code)
+
+@bot.command(name='echo')
+async def echo(ctx):
+    response = ctx.message.content.replace(ctx.message.content.split()[0] + " ", '')
+    await ctx.send(response)
+
+
+# Shut down
+@bot.command(name='quit')
+async def quit(ctx):
+    response = 'Shutting down...'
+    await ctx.send(response)
+    await bot.logout()
+
+# run bot
 bot.run(TOKEN)
