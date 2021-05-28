890afa5cd9db606a9f15542cb8d98dbacaca6a99
Noah <noahpdx@gmail.com>
2021-05-28 03:26:42 -0700

    Added "Who's that Pokemon?" command

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import discord
 import requests
 import json
 import random as rand
+from PIL import Image
+
 
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -22,11 +24,19 @@ async def on_ready():
 # Commands
 @bot.command(name='ping')
 async def ping(ctx):
+    """
+    Ping
+    """
     response = 'Ready to go!'
     await ctx.send(response)
 
 @bot.command(name='dex')
 async def pokemon_search(ctx):
+    """
+    Search for a Pokedex entry
+
+    Dexter Bot looks up a Pokemon based on your input, by either National Dex number or name
+    """
     search_term = ctx.message.content.replace(ctx.message.content.split()[0] + " ", '')
     if search_term.isnumeric():
         if int(search_term) > 898:
@@ -79,6 +89,12 @@ async def pokemon_search(ctx):
 
 @bot.command(name='rdex')
 async def random_pokemon(ctx):
+    """
+    Random Pokedex entry
+
+    Dexter Bot looks up a random Pokemon
+    """
+
     rand.seed()
     random_num = str(rand.randint(1, 898))
     
@@ -123,9 +139,50 @@ async def random_pokemon(ctx):
         await ctx.send(dex_text)
         await ctx.send(image)
 
+@bot.command(name='wtp')
+async def wtp(ctx):
+    """
+    Who's that Pokemon?
+    """
+    
+    rand.seed()
+    random_num = str(rand.randint(1, 898))
+    
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/'+random_num)
+    if response.status_code > 400:
+        await ctx.send('Connection failed.')
+        fail_string = 'Status codes: '+str(response.status_code)
+        await ctx.send(fail_string)
+    elif response.status_code == 200:
+        jsondata = json.loads(response.text)
+        name = jsondata['name'].replace('-', ' ')
+        image_url = jsondata['sprites']['other']['official-artwork']['front_default']
+        
+        image = Image.open(requests.get(image_url, stream=True).raw)
+        alpha = image.getchannel('A')
+        silhouette = Image.new('RGBA', image.size, color='black')
+        silhouette.putalpha(alpha)
+        silhouette.save("silhouette.png")
+
+        await ctx.send("**Who's that Pokemon?**")
+        await ctx.send(file=discord.File('silhouette.png'))
+        os.remove("silhouette.png")
+        #print(name)
+
+        def check(message):
+            return name in message.content.lower() and message.channel == ctx.channel
+        
+        msg = await bot.wait_for("message", check=check)
+        await ctx.send(f"{msg.author.mention} got it right! It's {name.capitalize()}!")
+        await ctx.send(image_url)
+
+
 # Shut down
 @bot.command(name='q')
 async def quit(ctx):
+    """
+    Shuts down Dexter Bot
+    """
     response = 'Shutting down...'
     await ctx.send(response)
     await bot.close()
